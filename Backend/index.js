7700c3f9ae09c0632dab495bdc0f8577dec59f9c
shubham <shubhamuprade0@gmail.com>
2025-08-09 14:50:46 +0530

    f-b

--- Backend/index.js
+++ Backend/index.js
@@ -17,9 +17,8 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5000',
-  // 'https://your-frontend-domain.com', // change to your frontend
+  'https://code-genisis-front.vercel.app/',
+  'https://code-genisis.vercel.app/',
 ];
 
 app.use(cors({
